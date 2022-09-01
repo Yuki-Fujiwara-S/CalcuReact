9f2b7fd0a5773669b1744f3432d3f550397126ec
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-01 10:23:23 -0700

    add more styling to button

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span>0
+          <span>(0) </span>0
         </div>
 
         <div className="operators">
